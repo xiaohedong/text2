6dad2e44c306367eb52000dfa6fcebaa8581237d
hongbaodai <dongxiaohe@hongbaodai.com>
2017-09-26 11:35:58 +0800

    Initial Commit

---
+++ Text2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Text2
+//
+//  Created by hongbaodai on 2017/9/26.
+//  Copyright © 2017年 hongbaodai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Text2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Text2
+//
+//  Created by hongbaodai on 2017/9/26.
+//  Copyright © 2017年 hongbaodai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
